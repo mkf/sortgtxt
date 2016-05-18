41378ff15482a38529b5fc943b061877a634c123
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-05-18 20:45:50 +0200

    Basic entry parsing

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 import re
 
 quot = re.compile('"([^"]*)"')
+msgstrbracke = re.compile('msgstr\[(\d*)]')
 
 
 def jakbyco(linie):
@@ -8,6 +9,11 @@ def jakbyco(linie):
         if len(l) == 0:
             raise PustaLinia
     k = ""
+    transcomme = []
+    extracomme = []
+    refere = []
+    flagslines = []
+    previouscomme = []
     for l in linie:
         if l.startswith('"'):
             if k == "msgid":
@@ -18,8 +24,10 @@ def jakbyco(linie):
                 msgstr += quot.search(l)
             elif k == "msgstr[":
                 a = msgstrlist.pop()
-                a+=quot.search(l)
+                a += quot.search(l)
                 msgstrlist.append(a)
+            else:
+                raise UntiedQuote
 
         elif l.startswith("msgid "):
             msgid = quot.search(l)
@@ -34,11 +42,25 @@ def jakbyco(linie):
             if not mamyliste:
                 mamyliste = true
                 msgstrlist = []
-            msgstrlist.append(quot.search(l))
+            msgstrlist.append((msgstrbracke.search(l), quot.search(l)))
             k = "msgstr["
+        elif l.startswith("# "):
+            transcomme.append(l)
+        elif l.startswith("#."):
+            extracomme.append(l)
+        elif l.startswith("#:"):
+            refere.append(l)
+        elif l.startswith("#,"):
+            flagslines.append(l)
+        elif l.startswith("#|"):
+            previouscomme.append(l)
 
 
 class PustaLinia(Exception):
+    pass
+
+
+class UntiedQuote(Exception):
     pass
 
 
