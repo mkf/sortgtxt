2664a05e5a1ceeaf984ed77cc8131a9d2d879690
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-06-16 15:15:00 +0200

    testing mixed, less debug

--- classes.py
+++ classes.py
@@ -192,10 +192,18 @@ class TildedComment(Comment):
 
 with open("django.po") as f:
     a = Baza(f)
-    print(a.wpisy)
+
+with open("djangomixed.po") as g:
+    b = Baza(g)
 
 with open("docel.po", "w") as tar:
     tar.truncate()
     a.sortbymsgid()
     a.rawzapisdopliku(tar)
     tar.close()
+
+with open("docelmixed.po", "w") as toa:
+    toa.truncate()
+    b.sortbymsgid()
+    b.rawzapisdopliku(toa)
+    toa.close()
