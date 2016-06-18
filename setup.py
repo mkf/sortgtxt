74926aeaee5ff795300c88f031931be8c708422c
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-06-18 08:47:01 +0200

    Added utf-8 encoding declarations, also py versions setup.py classifiers

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
@@ -14,6 +16,15 @@ setup(
     url='https://github.com/ArchieT/sortgtxt',
     author="Michał Krzysztof Feiler",
     author_email="archiet@platinum.edu.pl",
+    classifiers=[
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+    ],
     packages=find_packages(),
     install_requires=["babel", "re"],
 )
